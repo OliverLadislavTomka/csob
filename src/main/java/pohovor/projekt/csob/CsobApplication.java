7295639fa07c9e8be452f2713bf36289186310af
OliLaTom <oliver.tomka@ekom21.de>
2023-02-15 21:31:21 +0100

    init project

---
+++ src/main/java/pohovor/projekt/csob/CsobApplication.java
@@ -0,0 +1,13 @@
+package pohovor.projekt.csob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pohovor/projekt/csob/CsobApplicationTests.java
@@ -0,0 +1,13 @@
+package pohovor.projekt.csob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
